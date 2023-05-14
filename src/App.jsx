abfc0a95b9987408189ade38a79a903fde2be9bf
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-14 23:54:36 +0200

    move main section to react app

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,45 @@ function App() {
         <Button type="button"
           onClick="document.getElementById('clock3').src='analogclock.svg#2022-01-01T18:00'">18:00</Button>
       </TopPanel>
-      <AnalogClockFrame></AnalogClockFrame>
+
+      <main class="clearfix">
+        <section class="column">
+          <div class="analog-clock">
+            <AnalogClockFrame></AnalogClockFrame>
+          </div>
+
+          <form class="digital-clock">
+            <table>
+              <tr>
+                <td><button type="button">+</button></td>
+                <td></td>
+                <td><button type="button">+</button></td>
+                <td><button type="button">+</button></td>
+              </tr>
+
+              <tr class="display">
+                <td><input type="number" min="0" max="23" step="1" value="10" /></td>
+                <td>:</td>
+                <td colspan="2"><input type="number" min="0" max="60" step="1" value="33" /></td>
+              </tr>
+
+              <tr style={{ verticalAlign: 'top' }}>
+                <td><button type="button">-</button></td>
+                <td></td>
+                <td><button type="button">-</button></td>
+                <td><button type="button">-</button></td>
+              </tr>
+            </table>
+          </form>
+        </section>
+
+        <section class="column">
+          <form class="notes">
+            <label for="notes">Notes:</label><br />
+            <textarea id="notes"></textarea>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
