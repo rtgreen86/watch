bdf3322562d47391346c3dcd9c3199ee5c438bda
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-14 23:56:46 +0200

    move analog clock styles to component

--- src/AnalogClockFrame.jsx
+++ src/AnalogClockFrame.jsx
@@ -1,3 +1,5 @@
+import './AnalogClockFrame.css';
+
 export default function AnalogClockFrame() {
   return (<div class="analog-clock">
     <iframe id="clock3" src='analogclock.svg' title="clock"></iframe>
