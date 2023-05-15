80dede54e17a50e92ff2d6188f50a3c81d3745b1
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-15 21:16:55 +0200

    cleanup

--- src/Button.jsx
+++
@@ -1,3 +0,0 @@
-export default function Button({ children = 'Button' }) {
-  return <button type="button">{ children }</button>
-}
--- src/Checkbox.jsx
+++
@@ -1,3 +0,0 @@
-export default function Checkbox({ checked, children }) {
-  return <><input type="checkbox" checked={checked}></input><label for="hours">{children}</label></>;
-}
